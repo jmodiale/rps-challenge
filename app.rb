d534d94559c7d58052189cfb74afc772cf879831
jmodiale <remen.omodiale@outlook.com>
2022-01-10 09:48:10 +0000

    user-story 1

--- app.rb
+++ app.rb
@@ -10,5 +10,14 @@ class RPS < Sinatra::Base
     erb(:index)
   end
 
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end
+
+  get 'play' do
+    erb(:play)
+  end
+
   run! if app_file == $0
 end
